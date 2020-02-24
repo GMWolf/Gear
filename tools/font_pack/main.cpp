a51851a33d8bf9028b44020349521e1354ddd112
felix <felixbridault@gmail.com>
2020-02-24 21:18:54 +0000

    font packer

--- tools/font_pack/main.cpp
+++ tools/font_pack/main.cpp
@@ -11,6 +11,13 @@
 #include <stb_image_write.h>
 #include <vector>
 #include <string>
+#include <yaml-cpp/yaml.h>
+#include <filesystem>
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+namespace fs = std::filesystem;
+namespace nj = nlohmann;
 
 struct FontChar {
     float advance;
@@ -47,29 +54,61 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    const unsigned char* ttf_data = (unsigned char*)stb_file(const_cast<char*>(inputPath.c_str()), nullptr);
+    std::string bitmapOut = outFileName + ".png";
+    std::string jsonOut = outFileName + ".json";
+
+    const auto config = YAML::LoadFile(inputPath);
+
+    auto bitmapWidth = config["bitmap_width"].as<int>();
+    auto bitmapHeight = config["bitmap_height"].as<int>();
+    auto lineHeight = config["line_height"].as<float>();
+    auto rangeStart = config["range_start"].as<int>();
+    auto rangeCount = config["range_count"].as<int>();
+
+    auto pathRelDir = fs::path(inputPath).parent_path();
+    auto file = pathRelDir / config["font"].as<std::string>();
+
+    const unsigned char* ttf_data = (unsigned char*)stb_file(const_cast<char*>(file.c_str()), nullptr);
 
     if (!ttf_data) {
-        std::cerr << "could not open " << inputPath.c_str() << "\n";
+        std::cerr << "could not open " << file.c_str() << "\n";
         return 1;
     }
 
-    int bitmapWidth = 512;
-    int bitmapHeight = 512;
-    float lineHeight = 32.0f;
-
     auto* bitmap = static_cast<unsigned char*>(calloc(bitmapWidth * bitmapHeight, sizeof(unsigned char)));
 
-    stbtt_packedchar pdata[256*2];
+    stbtt_packedchar pdata[256];
     memset(pdata, 0, sizeof(pdata));
     stbtt_pack_context spc;
     stbtt_PackBegin(&spc, bitmap, bitmapWidth, bitmapHeight, 0, 1, nullptr);
     stbtt_PackSetOversampling(&spc, 1, 1);
-    stbtt_PackFontRange(&spc, ttf_data, 0, lineHeight, 32, 95, pdata);
+    stbtt_PackFontRange(&spc, ttf_data, 0, lineHeight, rangeStart, rangeCount, pdata);
     stbtt_PackEnd(&spc);
-    stbi_write_png("out.png", bitmapWidth, bitmapHeight, 1, bitmap, 0);
+    stbi_write_png(bitmapOut.c_str(), bitmapWidth, bitmapHeight, 1, bitmap, 0);
 
-    for(int i = 0; i < 95; i++) {
 
+    nj::json j;
+    j["texture"] = bitmapOut;
+    j["glyphs"] = {};
+    j["rangeStart"] = rangeStart;
+    j["rangeCount"] = rangeCount;
+    auto& glyphs = j["glyphs"];
+    for(int i = 0; i < rangeCount; i++) {
+        nj::json g;
+        g["x0"] = pdata[i].x0;
+        g["x1"] = pdata[i].x1;
+        g["y0"] = pdata[i].y0;
+        g["y1"] = pdata[i].y1;
+        g["xadvance"] = pdata[i].xadvance;
+        g["xoff"] = pdata[i].xoff;
+        g["yoff"] = pdata[i].yoff;
+        g["xoff2"] = pdata[i].xoff2;
+        g["yoff2"] = pdata[i].yoff2;
+        glyphs.push_back(g);
+    }
+
+    {
+        std::ofstream ofs(jsonOut);
+        ofs << std::setw(4) << j << std::endl;
     }
 }
