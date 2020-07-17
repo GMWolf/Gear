257dc71cb3bfb97495f730b644509e87243595df
felix <felixbridault@gmail.com>
2020-07-17 12:11:17 +0100

    tools texture lib start

---
+++ tools/texture/texture.cpp
@@ -0,0 +1,37 @@
+//
+// Created by felix on 17/07/2020.
+//
+
+#include "texture.h"
+#include <lz4hc.h>
+
+namespace gear {
+
+    static size_t getFormatSize(gear::assets::PixelFormat format) {
+        switch (format) {
+            case gear::assets::PixelFormat_R8:
+                return 1;
+            case gear::assets::PixelFormat_RGB8:
+                return 3;
+            case gear::assets::PixelFormat_RGBA8:
+                return 4;
+        }
+    }
+
+    flatbuffers::Offset<gear::assets::Texture>
+    buildTexture(flatbuffers::FlatBufferBuilder fbb, int width, int height, gear::assets::PixelFormat format,
+                 const uint8_t *data) {
+
+        size_t dataSize = width * height * getFormatSize(format);
+
+        std::vector<uint8_t> compressedData(LZ4_compressBound(dataSize));
+
+        auto compressedSize = LZ4_compress_HC(reinterpret_cast<const char *>(data),
+                                              reinterpret_cast<char *>(compressedData.data()), dataSize,
+                                              compressedData.size(), LZ4HC_CLEVEL_MAX);
+        compressedData.resize(compressedSize);
+
+        return gear::assets::CreateTextureDirect(fbb, width, height, format, &compressedData);
+    }
+
+}
---
+++ tools/texture/texture.h
@@ -0,0 +1,18 @@
+//
+// Created by felix on 17/07/2020.
+//
+
+#ifndef TBTG_TEXTURE_H
+#define TBTG_TEXTURE_H
+
+#include <gear/fbs/generated/texture_generated.h>
+#include <vector>
+
+namespace gear {
+
+    flatbuffers::Offset<gear::assets::Texture>
+    buildTexture(flatbuffers::FlatBufferBuilder builder, int width, int height, gear::assets::PixelFormat format,
+                 const uint8_t *data);
+
+}
+#endif //TBTG_TEXTURE_H
